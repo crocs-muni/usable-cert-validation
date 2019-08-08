8639dc95e276aeac79d9b32c4a5a0a2c160541ee
Martin Ukrop <mukrop@mail.muni.cz>
2019-08-08 14:57:33 +0200

    Improve style

---
+++ web/assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(document).scroll(function() {
+        var scrolled = $(this).scrollTop();
+        if(scrolled > 50) {
+            $(".navbar").addClass('navbar-scrolled');
+        } else {
+            $(".navbar").removeClass('navbar-scrolled');
+        }
+    });
+});
