af2430a0575591d964f19afc0cd4355041634f18
Matěj Grabovský <matej@mgrabovsky.net>
2020-05-05 19:46:34 +0200

    Expand error details by default
    Expand "rolldowns" with error details by default so that they are
    readable in browsers with JS disabled. Collapse them once the page loads
    in JavaScript.
    
    We do this by hand, since Bootstrap animates the collapsing, which takes
    some time, which is undesirable on page load.
    
    Resolves #41

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    // Collapse error details.
+    $('.collapse.show')
+        .removeClass('show')
+        .siblings('.card-header')
+        .addClass('collapsed');
+
+    // Add shadow under navbar if the page is scrolled already on load.
     $(document).scroll(function() {
         var scrolled = $(this).scrollTop();
         if(scrolled > 50) {
