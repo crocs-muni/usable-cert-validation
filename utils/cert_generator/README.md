c112a33d1f084626dc6db3ea79cdc536a593e87f
Pavol Zacik <xzacik@fi.muni.cz>
2020-09-24 09:26:37 +0200

    Improve certificate template and types

--- utils/cert_generator/README.md
+++ utils/cert_generator/README.md
@@ -23,4 +23,53 @@ __$ ./cert_generator -signingKey key_file --templateFile template --outFile out.
 
 *outFile* is a filename, where the resulting certificate will be written in PEM form
 
+### Supported template types and equivalent ASN.1 tags
+
+
+- SET is equivalent to ASN.1 SET, a structured type. \
+It must be represented as a YAML mapping. \
+Duplicate mapping keys are allowed in this context.
+
+- SEQUENCE is equivalent to ASN.1 SEQUENCE, a structured type. \
+It must be represented as an ordered YAML mapping. \
+Duplicate mapping keys are allowed in this context. 
+
+- INTEGER is equivalent to ASN.1 INTEGER. \
+It must be represented as an integer that can be parsed as golang's **int**.
+
+- BOOLEAN is equivalent to ASN.1 BOOLEAN. \
+Possible values are **true** or **false**.
+
+- IA5STRING is equivalent to ASN.1 IA5String. \
+Possible values are strings containing only ISO 646 (IA5) characters.
+
+- UTF8STRING is equivalent to ASN.1 UTF8String. \
+Possible values are strings containing UTF8 characters.
+
+- PRTINTABLESTRING is equivalent to ASN.1 PrintableString. \
+Possible values are strings containing only printable ASCII characters.
+
+- DATE is a type which generates ASN.1 GeneralizedTime. \
+Valid date must be in the form **YYYY/MM/DD**.
+
+- YEAROFFSET is a type which generates ASN.1 GeneralizedTime \
+Any **int** is a valid value. The generated time is the current time + number of years given by the integer.
+
+- OID is equivalent to ASN.1 OBJECT IDENTIFIER. \
+Possible values are arbitrary number of digits separated by commas arbitrarily.
+
+- NULL_TAG is equivalent to ASN.1 NULL. \
+The only meaningful value is NULL, as it is ignored.
+
+- OCTETSTRING is equivalent to ASN.1 OCTETSTRING. \
+
+- OCTETCAPSULE generates an ASN.1 OCTETSTRING. \
+It must be represented as a mapping and it creates a DER capsule around the types underneath. 
+
+- ISSUERCERT is a special type used to create RFC5280 certificate issuer value. \ 
+Valid value is a filename of a PEM encoded issuer certificate.
+ 
+- PRIVATEKEY is a special type used to create RFC5280 PublicKeyInfo certificate value. \
+Valid value is a filename of a PEM encoded private key complementary to the public one. 
+
  
--- utils/cert_generator/asn1-builder.go
+++ utils/cert_generator/asn1-builder.go
@@ -9,109 +9,162 @@ import (
 	"time"
 )
 
+func parseName(name string) (string, string, int, error) {
+	data := strings.Split(name, " ")
+
+	if len(data) == 1 {
+		return data[0], "", 0, nil
+	}
+
+	number, err := strconv.Atoi(data[2])
+	if err != nil {
+		return "", "", 0, err
+	}
+
+	return data[0], data[1], number, nil
+}
+
+func parseOID(data string) ([]int, error) {
+	numbers := strings.Split(data, ".")
+	var oid []int
+	for _, c := range numbers {
+		next, err := strconv.Atoi(c)
+		if err != nil {
+			return nil, err
+		}
+		oid = append(oid, next)
+	}
+	return oid, nil
+}
+
+func buildCapsule(obj *Object) ([]byte, error) {
+	var b cryptobyte.Builder
+	for _, ch := range obj.Children {
+		err := BuildASN1(&ch, &b)
+		if err != nil {
+			return nil, err
+		}
+	}
+	bytes, _ := b.Bytes()
+	return bytes, nil
+}
+
+func buildPublicKeyInfo(filename string) ([]byte, error) {
+	key, err := LoadPrivateKey(filename)
+	if err != nil {
+		return nil, err
+	}
+	var subB cryptobyte.Builder
+	subB.AddASN1(cryptobyteasn1.SEQUENCE, func(b *cryptobyte.Builder) {
+		b.AddASN1BigInt(key.N, nil)
+		b.AddASN1Int64(int64(key.E), nil)
+	})
+	bytes, _ := subB.Bytes()
+	return bytes, nil
+}
+
 // Builds ASN.1 DER data from a given Object
 func BuildASN1(obj *Object,  builder *cryptobyte.Builder) error {
-	name := splitType(obj.Name)
-	switch name[0] {
-	case "SEQUENCE":
-		builder.AddASN1(cryptobyteasn1.SEQUENCE, func(b *cryptobyte.Builder) {
-			for _, ch := range obj.Children {
-				err := BuildASN1(&ch, b)
-				if err != nil {
-					panic(err)
-				}
-			}
-		})
-	case "SET":
-		builder.AddASN1(cryptobyteasn1.SET, func(b *cryptobyte.Builder) {
-			for _, ch := range obj.Children {
-				err := BuildASN1(&ch, b)
-				if err != nil {
-					panic(err)
-				}
-			}
-		})
-	case "BOOLEAN":
-		boolValue, _ := strconv.ParseBool(obj.Content)
-		builder.AddASN1Boolean(boolValue)
-	case "INTEGER":
-		intValue, _ := strconv.Atoi(obj.Content)
-		builder.AddASN1Int64(int64(intValue))
-	case "IA5String":
-		builder.AddASN1(cryptobyteasn1.Tag(22), func(b *cryptobyte.Builder) {
-			b.AddBytes([]byte(obj.Content))
-		})
-	case "UTF8String":
-		builder.AddASN1(cryptobyteasn1.Tag(12), func(b *cryptobyte.Builder) {
-			b.AddBytes([]byte(obj.Content))
-		})
-	case "PrintableString":
-		builder.AddASN1(cryptobyteasn1.Tag(19), func(b *cryptobyte.Builder) {
-			b.AddBytes([]byte(obj.Content))
-		})
-	case "OID":
-		numbers := strings.Split(obj.Content, ".")
-		var bytes []int
-		for _, c := range numbers {
-			next, _ := strconv.Atoi(c)
-			bytes = append(bytes, next)
-		}
-		builder.AddASN1ObjectIdentifier(bytes)
-	case "NULL_TAG":
-		builder.AddASN1NULL()
-	case "TIME":
-		t, _ := time.Parse("2006/01/02", obj.Content)
-		builder.AddASN1GeneralizedTime(t)
-	case "YEAR_OFFSET":
-		years, _ := strconv.Atoi(obj.Content)
-		t := time.Now().AddDate(years, 0, 0)
-		builder.AddASN1GeneralizedTime(t)
-	case "BITSTRING":
-		builder.AddASN1BitString([]byte(obj.Content))
-	case "OCTETSTRING":
-		var subB cryptobyte.Builder
+
+	buildChildren := func(b *cryptobyte.Builder) {
 		for _, ch := range obj.Children {
-			err := BuildASN1(&ch, &subB)
+			err := BuildASN1(&ch, b)
 			if err != nil {
-				return err
+				panic(err)
 			}
 		}
-		bytes, _ := subB.Bytes()
-		builder.AddASN1OctetString(bytes)
-	case "PRIVATE_KEY_FILE":
-		key, err := LoadPrivateKey(obj.Content)
+	}
+
+	typename, tag, number, err := parseName(obj.Name)
+	if err != nil {
+		return err
+	}
+
+	if tag == "EXPLICIT" {
+		builder.AddASN1(cryptobyteasn1.Tag(number).ContextSpecific().Constructed(), func(b *cryptobyte.Builder) {
+				obj.Name = typename
+				err := BuildASN1(obj, b)
+				if err != nil {
+					panic(err)
+				}
+		})
+		return nil
+	}
+
+	var implicit *cryptobyteasn1.Tag
+	implicit = nil
+
+	if tag == "IMPLICIT" {
+		t := cryptobyteasn1.Tag(number).ContextSpecific()
+		implicit = &t
+	}
+
+	switch typename {
+	case "SEQUENCE":
+		builder.AddASN1(cryptobyteasn1.SEQUENCE, buildChildren)
+	case "SET":
+		builder.AddASN1(cryptobyteasn1.SET, buildChildren)
+	case "BOOLEAN":
+		boolValue, err := strconv.ParseBool(obj.Content)
 		if err != nil {
 			return err
 		}
-		var subB cryptobyte.Builder
-		subB.AddASN1(cryptobyteasn1.SEQUENCE, func(b *cryptobyte.Builder) {
-			b.AddASN1BigInt(key.N)
-			b.AddASN1Int64(int64(key.E))
-		})
-		res, _ := subB.Bytes()
-		builder.AddASN1BitString(res)
-	case "EXPLICIT":
-		tag, _ := strconv.Atoi(name[1])
-		builder.AddASN1(cryptobyteasn1.Tag(tag).ContextSpecific().Constructed(),func(b *cryptobyte.Builder) {
-			for _, ch := range obj.Children {
-				err := BuildASN1(&ch, b)
-				if err != nil {
-					panic(err)
-				}
-			}
-		})
-	case "IMPLICIT":
-		tag, _ := strconv.Atoi(name[1])
-		builder.AddASN1(cryptobyteasn1.Tag(tag).ContextSpecific(), func(b *cryptobyte.Builder) {
-			b.AddBytes([]byte(obj.Content))
-		})
+		builder.AddASN1Boolean(boolValue, implicit)
+	case "INTEGER":
+		intValue, err := strconv.Atoi(obj.Content)
+		if err != nil {
+			return err
+		}
+		builder.AddASN1Int64(int64(intValue), implicit)
+	case "IA5STRING":
+		builder.AddASN1IA5String(obj.Content, implicit)
+	case "UTF8STRING":
+		builder.AddASN1UTF8String(obj.Content, implicit)
+	case "PRINTABLESTRING":
+		builder.AddASN1PrintableString(obj.Content, implicit)
+	case "OID":
+		oid, err := parseOID(obj.Content)
+		if err != nil {
+			return err
+		}
+		builder.AddASN1ObjectIdentifier(oid, implicit)
+	case "NULLTAG":
+		builder.AddASN1NULL()
+	case "DATE":
+		t, err := time.Parse("2006/01/02", obj.Content)
+		if err != nil {
+			return err
+		}
+		builder.AddASN1GeneralizedTime(t, implicit)
+	case "YEAROFFSET":
+		years, err := strconv.Atoi(obj.Content)
+		if err != nil {
+			return err
+		}
+		t := time.Now().AddDate(years, 0, 0)
+		builder.AddASN1GeneralizedTime(t, implicit)
+	case "OCTETCAPSULE":
+		bytes, err := buildCapsule(obj)
+		if err != nil {
+			return err
+		}
+		builder.AddASN1OctetString(bytes, implicit)
+	case "OCTETSTRING":
+		builder.AddASN1OctetString([]byte(obj.Content), implicit)
+	case "PRIVATEKEY":
+		bytes, err := buildPublicKeyInfo(obj.Content)
+		if err != nil {
+			return err
+		}
+		builder.AddASN1BitString(bytes, implicit)
+	case "ISSUERCERT":
+		cert, err := LoadCertificate(obj.Content)
+		if err != nil {
+			return err
+		}
+		builder.AddBytes(cert.RawSubject)
 	default:
-		return errors.New("unknown type")
+		return errors.New("unknown type " + obj.Name)
 	}
 	return nil
-}
-
-// Splits a string by a delimiter "-", used in type names
-func splitType(name string) []string {
-	return strings.Split(name, "-")
 }
