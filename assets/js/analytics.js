60f9706e5a5f3dfafb894f2cd5237204fc4100fd
Martin Ukrop <mukrop@mail.muni.cz>
2021-11-28 12:37:39 +0100

    Move analytics to separate file, don't run on dev version
    * Separate file enables cleaner CSP.

---
+++ assets/js/analytics.js
@@ -0,0 +1,5 @@
+// Matomo Tag Manager
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.type='text/javascript'; g.async=true; g.src='https://fadmin.fi.muni.cz/piwik/js/container_PX176f6U.js'; s.parentNode.insertBefore(g,s);
